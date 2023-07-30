6149a8b75c3b0de453d575949d76c35a3951cddc
optimisthorses <juanpabernal20011@gmail.com>
2023-07-29 19:29:50 -0500

    inicio de sesion con credenciales exitoso

--- app.py
+++ app.py
@@ -1,53 +1,60 @@
 from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'w83aJSWWfVLzE6'
+app.config['MYSQL_DB'] = 'usuarios_spotify'
 
-# Ruta de inicio
+mysql = MySQL(app)
+
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-# Ruta de registro
+
 @app.route('/registro', methods=['GET', 'POST'])
 def registro():
     if request.method == 'POST':
         usuario = request.form['usuario']
         contrasena = request.form['contrasena']
 
-        # Aquí puedes agregar la lógica para guardar los datos en la base de datos
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO usuarios (usuario, contrasena) VALUES (%s, %s)", (usuario, contrasena))
+        mysql.connection.commit()
+        cur.close()
 
-        # Redireccionar a la página de inicio de sesión con mensaje de registro exitoso
-        return redirect('/inicio_sesion?registro=exitoso')
+        registrado = True  # Variable para indicar registro exitoso
+
+        return redirect('/inicio_sesion?registrado=true')
 
     return render_template('registro.html')
 
-# Ruta de inicio de sesión
+
 @app.route('/inicio_sesion', methods=['GET', 'POST'])
 def inicio_sesion():
-    # Verificar si se pasó un mensaje de registro exitoso en la URL
-    registro_exitoso = request.args.get('registro')
+    registrado = request.args.get('registrado')  # Obtener el valor de la variable registrado
 
     if request.method == 'POST':
         usuario = request.form['usuario']
         contrasena = request.form['contrasena']
 
-        # Aquí puedes agregar la lógica para autenticar los datos en la base de datos
-        if usuario == 'usuario' and contrasena == 'contrasena':
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM usuarios WHERE usuario = %s AND contrasena = %s", (usuario, contrasena))
+        usuario_encontrado = cur.fetchone()
+        cur.close()
+
+        if usuario_encontrado:
             # Iniciar sesión exitosa
-            return redirect('/inicio?login=exitoso')
+            # Realiza las acciones necesarias, como establecer una sesión
+            return redirect('/inicio')
         else:
             # Credenciales incorrectas
-            return redirect('/inicio_sesion?login=fallido')
+            return "Credenciales incorrectas"
 
-    return render_template('inicio_sesion.html', registro_exitoso=registro_exitoso)
-
-# Ruta de inicio de sesión exitoso
-@app.route('/inicio', methods=['GET'])
-def inicio():
-    # Verificar si se pasó un mensaje de inicio de sesión exitoso en la URL
-    login_exitoso = request.args.get('login')
-
-    return render_template('inicio.html', login_exitoso=login_exitoso)
+    return render_template('inicio_sesion.html', registrado=registrado)
 
 
 if __name__ == '__main__':
